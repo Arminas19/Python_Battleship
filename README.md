3130b11bc2a214aa167f3f83acedd47ae94b7139
Arminas19 <armeska14@gmail.com>
2021-10-26 16:12:32 +0000

    add game_board function which creates the game board

---
+++ README.md
@@ -0,0 +1,2 @@
+Link - Type of content
+https://www.youtube.com/watch?v=cwpS_ac8uk0 - How to make a board.
--- main.py
+++ main.py
@@ -1,6 +1,66 @@
-import random
+from random import randint
 
 player_score = 0
 computer_score = 0
+board = []
 
 
+class Setup_Board:
+    def __init__(self, num_ships, user_name, type):
+        self.num_ships = num_ships
+        self.user_name = user_name
+        self.type = type 
+    
+    
+    
+
+def game_board(board):
+    """
+    Creates the game board that the user can play in.
+    """
+    print('Enter Your Name: ')
+    print('\n\n\n')
+    input('          ')
+    print('    0 1 2 3 4 5 6 7 8 9')
+    print('  +---------------------+')
+    for row in range(10):
+        board.append(['-'] * 10)
+    
+    letters = 0
+    for row in range(10):
+        print(chr(letters + 65), end=' | ') 
+        for column in range(len(board[letters])):
+            print(board[letters][column], end=' ')
+        print('| ')
+        letters += 1
+    print('  +---------------------+ \n\n\n')
+
+    print('         COMPUTER ')
+    print('    0 1 2 3 4 5 6 7 8 9')
+    print('  +---------------------+')
+    for row in range(10):
+        board.append(['-'] * 10)
+         
+    letters = 0
+    for row in range(10):
+        print(chr(letters + 65), end=' | ') 
+        for column in range(len(board[letters])):
+            print(board[letters][column], end=' ')
+        print('| ')
+        letters += 1
+    print('  +---------------------+ \n\n\n')
+
+
+def New_game():
+    """
+    Starts a new game and welcomes the user to the game.
+    """
+    print('Welcome To Battleship!! ')
+    print('How to play? Try and guess where the')
+    print('enemy ships are by choosing a row and column.')
+    print('Example: row: 2  comlumn: D \n \n\n')
+
+    game_board(board)
+
+
+New_game()
