d58970fb12450d4e7f4604256a1b6c98a389ea37
Arminas19 <armeska14@gmail.com>
2021-11-02 15:37:11 +0000

    vaildate Corordinates updated, put all code together inside one while loop

--- Test.py
+++ Test.py
@@ -1,11 +1,45 @@
-import random
+num_ships = 5
+computer_board = []
 
-array = ['orange', 'apple', 'starwbery', 'lemon', 'grape', 'Bird', 'tomatoe']
-random_number = random.randrange(8)
+while num_ships > 0:
+    Alpabet = None
+    Alpabet = input('Enter a Row: ')
+    if Alpabet.isdigit() and int(Alpabet) >= 0 and int(Alpabet) <= 9:
+        Alpabet = int(Alpabet)
+        Numbers = None
+        Numbers = input('Enter a Column: ')
+        if Numbers.isdigit() and int(Numbers) >= 0 and int(Numbers) <= 9:
+            Numbers = int(Numbers)
+            if computer_board[Alpabet][Numbers] == 'O':
+                print('Hit')
+                num_ships -= 1
+                computer_board[Alpabet][Numbers] = 'X'
+            else:
+                if computer_board[Alpabet][Numbers] == 'X':
+                    print('Already picked this Corordinate, Please Pick again.')
+                else:
+                    if computer_board[Alpabet][Numbers] == '-':
+                        print('Missed')
+                        computer_board[Alpabet][Numbers] = 'X'
 
-while 5 > len(random_number):
-    array[random_number] = 'O'
-    break
+        else:
+            print('row and colum must be between 0 and 9, they must be an int')
+    else:
+        print('row and colum must be between 0 and 9, they must be an int')
+        
+"""
+    if computer_board[Alpabet][Numbers] == 'O':
+        print('Hit')
+        num_ships -= 1
+        computer_board[Alpabet][Numbers] = 'X'
+        # return 'Hit'
 
-
-print(array)
+    else:
+        if computer_board[Alpabet][Numbers] == 'X':
+            print('Already picked this Corordinate, Please Pick again.')
+        else:
+            if computer_board[Alpabet][Numbers] == ['-']:
+                print('Missed')
+                computer_board[Alpabet][Numbers] = 'X'
+                # return 'Missed'
+"""
--- main.py
+++ main.py
@@ -30,6 +30,34 @@ class Setup_Board:
         """
         Vaildates the corordinates that the Computer and Player Choose
         """
+        while num_ships > 0:
+
+            Alpabet = None
+            Alpabet = input('Enter a Row: ')
+            if Alpabet.isdigit() and int(Alpabet) >= 0 and int(Alpabet) <= 9:
+                Alpabet = int(Alpabet)
+                Numbers = None
+                Numbers = input('Enter a Column: ')
+                if Numbers.isdigit() and int(Numbers) >= 0 and int(Numbers) <= 9:
+                    Numbers = int(Numbers)
+                    if computer_board[Alpabet][Numbers] == 'O':
+                        print('Hit')
+                        num_ships -= 1
+                        computer_board[Alpabet][Numbers] = 'X'
+                    else:
+
+                        if computer_board[Alpabet][Numbers] == 'X':
+                            print('Already picked this Corordinate, Please Pick again.')
+                        else:
+                            if computer_board[Alpabet][Numbers] == '-':
+                                print('Missed')
+                                computer_board[Alpabet][Numbers] = 'X'
+
+                else:
+                    print('row and colum must be between 0 and 9, they must be an int')
+            else:
+                print('row and colum must be between 0 and 9, they must be an int')
+"""
         Alpabet = None
         while True:
             Alpabet = input('Enter a Row: ')
@@ -65,7 +93,7 @@ class Setup_Board:
                     print('Missed')
                     self.computer_board[Alpabet][Numbers] = 'X'
                     return 'Missed'
-
+"""
     def populate_boards(self, computer_board, board):
         """
         Populates the two boards
