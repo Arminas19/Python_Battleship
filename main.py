1de65d6182f4ad58f807aa38588aa8fcbbc55010
Arminas19 <armeska14@gmail.com>
2021-11-02 15:39:49 +0000

    added in self. to all the veriables in vaildate Corordinates method

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Setup_Board:
         """
         Vaildates the corordinates that the Computer and Player Choose
         """
-        while num_ships > 0:
+        while self.num_ships > 0:
 
             Alpabet = None
             Alpabet = input('Enter a Row: ')
@@ -40,24 +40,24 @@ class Setup_Board:
                 Numbers = input('Enter a Column: ')
                 if Numbers.isdigit() and int(Numbers) >= 0 and int(Numbers) <= 9:
                     Numbers = int(Numbers)
-                    if computer_board[Alpabet][Numbers] == 'O':
+                    if self.computer_board[Alpabet][Numbers] == 'O':
                         print('Hit')
-                        num_ships -= 1
-                        computer_board[Alpabet][Numbers] = 'X'
+                        self.num_ships -= 1
+                        self.computer_board[Alpabet][Numbers] = 'X'
                     else:
 
-                        if computer_board[Alpabet][Numbers] == 'X':
+                        if self.computer_board[Alpabet][Numbers] == 'X':
                             print('Already picked this Corordinate, Please Pick again.')
                         else:
-                            if computer_board[Alpabet][Numbers] == '-':
+                            if self.computer_board[Alpabet][Numbers] == '-':
                                 print('Missed')
-                                computer_board[Alpabet][Numbers] = 'X'
+                                self.computer_board[Alpabet][Numbers] = 'X'
 
                 else:
                     print('row and colum must be between 0 and 9, they must be an int')
             else:
                 print('row and colum must be between 0 and 9, they must be an int')
-"""
+        """
         Alpabet = None
         while True:
             Alpabet = input('Enter a Row: ')
@@ -93,7 +93,8 @@ class Setup_Board:
                     print('Missed')
                     self.computer_board[Alpabet][Numbers] = 'X'
                     return 'Missed'
-"""
+        """
+
     def populate_boards(self, computer_board, board):
         """
         Populates the two boards
