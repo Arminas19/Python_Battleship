4399c95960383aaa89ac0c475650299cc10dd7b2
Arminas19 <armeska14@gmail.com>
2021-11-05 14:59:14 +0000

    updated make_guess so that it loops everytime with new corordinates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 class Setup_Board:
     def __init__(self, num_ships, ship_hit, ship, type, guess, water,
-                 computer_board, board, player_name, row, column, alpabet, numbers):
+                 computer_board, board, player_name, row, column, alpabet, numbers, computer_board2):
         self.num_ships = 5
         self.ship_hit = 'X'
         self.type = type
@@ -15,8 +15,9 @@ class Setup_Board:
         self.player_name = player_name
         self.row = 9
         self.column = 9
+        self.computer_board2 = []
 
-    def validate_corordinates(self, player_name):
+    def validate_corordinates(self, player_name, alpabet, numbers):
         """
         Vaildates the corordinates that the Computer and Player Choose
         """
@@ -35,15 +36,16 @@ class Setup_Board:
                         print('You Hit')
                         self.num_ships -= 1
                         self.computer_board[self.alpabet][self.numbers] = 'X'
-                        self.render_computer_board('Computer')
+                        self.computer_board2[self.alpabet][self.numbers] = 'X'
+                        self.render_computer_board2(alpabet, numbers)
                         self.make_guess(player_name)
-                    elif self.computer_board[self.alpabet][self.numbers] == 'X':
+                    elif self.computer_board2[self.alpabet][self.numbers] == 'X':
                         print('Already picked this Corordinate, Please Pick again.')
                     else:
-                        if self.computer_board[self.alpabet][self.numbers] == '-':
+                        if self.computer_board2[self.alpabet][self.numbers] == '-':
                             print('You Missed')
-                            self.computer_board[self.alpabet][self.numbers] = 'X'
-                            self.render_computer_board('Computer')
+                            self.computer_board2[self.alpabet][self.numbers] = 'X'
+                            self.render_computer_board2(alpabet, numbers)
                             self.make_guess(player_name) 
                                            
                 else:
@@ -98,8 +100,9 @@ class Setup_Board:
                 break
                
             else:
-                self.board[computer_guess_x][computer_guess_y] == 'X'
-
+                computer_guess_x = random.randrange(10)
+                computer_guess_y = random.randrange(10)
+                                    
         if self.num_ships == 0:
             print('Computer has Won, starting new game')
             New_game()      
@@ -143,11 +146,31 @@ class Setup_Board:
             letters += 1
         print('  +---------------------+ \n\n\n')
 
+    def render_computer_board2(self, alpabet, numbers):
+        """
+        renders Computers Board
+        """
+        print('         COMPUTER ')
+        print('    0 1 2 3 4 5 6 7 8 9')
+        print('  +---------------------+')
+        for self.row in range(10):
+            self.computer_board2.append(self.water * 10)
+
+        letters = 0
+        for self.row in range(10):
+            print(chr(letters + 65), end=' | ')
+            for self.column in range(len(self.computer_board2[letters])):
+                print(self.computer_board2[letters][self.column], end=' ')
+            print('| ')
+            letters += 1
+        print('  +---------------------+ \n\n\n')
+
 
 def New_game():
     """
     Starts a new game and welcomes the user to the game.
     """
+    computer_board2 = []
     alpabet = None
     numbers = None
     # player_score = 9
@@ -170,7 +193,7 @@ def New_game():
     player_name = (input('Enter Your Name: '))
     print('-------------------------------------------\n\n\n')
     setup_pb = Setup_Board(num_ships, ship_hit, ship, type, guess, water,
-                           computer_board, board, player_name, row, column, alpabet, numbers)
+                           computer_board, board, player_name, row, column, alpabet, numbers, computer_board2)
     print('---------------------------------------------')
     print('This is what the boards look like.')
     setup_pb.render_Player_board('Player', player_name)
@@ -180,9 +203,10 @@ def New_game():
     print('---------------------------------------------')
     print('Game Starts Here')
     setup_pb.render_Player_board('Player', player_name)
-    setup_pb.render_computer_board('Computer')
+    # setup_pb.render_computer_board('Computer')
+    setup_pb.render_computer_board2(alpabet, numbers)
     setup_pb.make_guess(player_name)
-    setup_pb.validate_corordinates(player_name)
+    setup_pb.validate_corordinates(player_name, alpabet, numbers)
     
 
 New_game()
