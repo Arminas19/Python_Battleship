165cf6b9550e279e7e4775d3c5507963b173df5b
Arminas19 <armeska14@gmail.com>
2022-11-12 09:26:10 +0000

    created some functions and methods in main.py

--- main.py
+++ main.py
@@ -1,4 +1,75 @@
+from urllib.request import urlopen
 # text = input("Text: ")
 # emails = text.split(",")
 # output = "\n".join(emails)
 # print(output)
+
+
+# def square(x):
+#     return x*x
+
+
+# x = int(input("x:"))
+# sqr = square(x)
+# print(sqr)
+
+
+# def get_temperature(city):
+#     url = "http://wttr.in/" + city + "?format=%t"
+#     page = urlopen(url)
+#     raw = page.read()
+#     temp = raw.decode("utf-8")
+#     return temp
+
+# def gt3(x):
+#     if x > 3:
+#         return True
+#     else:
+#         return False
+
+# x = int(input("gt3 x: "))
+# gt3_function = gt3(x)
+# print(gt3_function)
+
+# def both_gt_3(x, c):
+#     return x > 3 and x > 3
+
+# x = int(input("x:"))
+# c = int(input("c:"))
+# both_gt_3_function = both_gt_3(x, c)
+# print(both_gt_3_function)
+
+
+# def sum3(lst_of_num):
+#     return sum(lst_of_num) * 3
+
+
+# lst_of_num = [2, 3, 5]
+# sum3_function = sum3(lst_of_num)
+# print(sum3_function)
+
+# def sumv(dict):
+#     val = dict.values()
+#     return sum(val)
+
+
+# dict = {"a": 4, "b": 5}
+# sumv_function = sumv(dict)
+# print(sumv_function)
+
+# def is_jack_one(dict):
+#     return dict["jack"] == 1
+
+
+# dict = {"jack": 1, "blob": 2, "fish": 15}
+# is_jack_one_function = is_jack_one(dict)
+# print(is_jack_one_function)
+
+# def add3_jack(dict):
+#     val = dict["jack"]
+#     return val + 3
+
+
+# dict = {"jack": 2, "blob": 2, "fish": 15}
+# add3_jack_function = add3_jack(dict)
+# print(add3_jack_function)
