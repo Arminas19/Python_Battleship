cbf5b340c4cf1ef472925d1790af2e11d3b3fbbb
Arminas19 <armeska14@gmail.com>
2022-11-16 14:59:01 +0000

    added a class for turning light switch on and off

--- learing.py
+++ learing.py
@@ -1,21 +1,43 @@
-class IceCream:
-    print('IceCream Class Working')
+# IceCream Class
+# class IceCream:
+#     print('IceCream Class Working')
 
-    def __init__(self):
-        self.scoops = 3
+#     def __init__(self):
+#         self.scoops = 3
 
-    def add(self, scoops):
-        self.scoops += scoops
+#     def add(self, scoops):
+#         self.scoops += scoops
 
-    def eat(self, scoops):
-        if self.scoops > scoops:
-            self.scoops -= scoops
-        else:
-            print("No More IceCream left! ")
+#     def eat(self, scoops):
+#         if self.scoops > scoops:
+#             self.scoops -= scoops
+#         else:
+#             print("No More IceCream left! ")
 
 
 # scoops = 3
 # ice_cream = IceCream()
 # ice_cream.eat(scoops)
 
+# Light Class
+# class light:
+#     def __init__(self):
+#         print('creating light! ')
+#         self.on = False
+    
+#     def toggle(self):
+#         print('Toggle on and off!')
+#         self.on = not self.on 
+    
+#     def is_on(self):
+#         if self.on == True:
+#             print("Light is on!")
+#         else:
+#             print("Light is off!")
 
+# Light = light()
+# Light.is_on()
+# # print("Light on?: ", Light.on)
+# Light.toggle()
+# # print("Light on?: ", Light.on)
+# Light.is_on()
