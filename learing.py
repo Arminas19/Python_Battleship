460c0251d1b92b649774e582901701ee15b00b17
Arminas19 <armeska14@gmail.com>
2022-11-18 11:41:54 +0000

    practiced abstraction methods in my iceCream class and created a new IceCreamTruck class

--- learing.py
+++ learing.py
@@ -1,23 +1,51 @@
 # IceCream Class
-# class IceCream:
-#     print('IceCream Class Working')
+class IceCream:
+    max_scoops = 3
 
-#     def __init__(self):
-#         self.scoops = 3
+    def __init__(self):
+        self.scoops = 3
 
-#     def add(self, scoops):
-#         self.scoops += scoops
+    def add(self, scoops):
+        self.scoops += scoops
+        if self.scoops > self.max_scoops:
+            self.scoops = 0
+            print("Too many scoops! Dropped ice cream.")
 
-#     def eat(self, scoops):
-#         if self.scoops > scoops:
-#             self.scoops -= scoops
-#         else:
-#             print("No More IceCream left! ")
+    def eat(self, scoops):
+        if self.scoops > scoops:
+            self.scoops -= scoops
+        else:
+            print("No More IceCream left! ")
 
 
-# scoops = 3
 # ice_cream = IceCream()
-# ice_cream.eat(scoops)
+# ice_cream.scoops += 2
+# ice_cream.add(2)
+# ice_cream.scoops -= 3
+# ice_cream.eat(3)
+
+# IceCreamTruck Class
+class IceCreamTruck:
+    def __init__(self):
+        self.sold = 0
+
+    def order(self, scoops):
+        ice_cream = IceCream()
+        self.add(ice_cream, scoops)
+        return ice_cream
+
+    def add(self, ice_cream, scoops):
+        ice_cream.add(scoops)
+        ice_cream.add(scoops)
+        self.sold += scoops
+
+truck = IceCreamTruck()
+ice_cream1 = truck.order(3)
+ice_cream1.eat(2)
+truck.add(ice_cream1, 1)
+print(truck.sold)
+
+
 
 # Light Class
 # class light:
@@ -43,14 +71,14 @@
 # Light.is_on()
 
 
-class Light:
-    on = False
+# class Light:
+#     on = False
 
 
-a = Light()
-a.on = True
-Light.on = True
-b = Light()
-print("b.on: ", b.on)
-print("a.on: ", a.on)
-print(Light.on)
+# a = Light()
+# a.on = True
+# Light.on = True
+# b = Light()
+# print("b.on: ", b.on)
+# print("a.on: ", a.on)
+# print(Light.on)
