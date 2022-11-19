20e72588387de6d11a5783a18d8f50c786e40954
Arminas19 <armeska14@gmail.com>
2022-11-19 11:51:47 +0000

    created a child class for the IceCreamTruck parent class.

--- learing.py
+++ learing.py
@@ -46,32 +46,32 @@
 # print(truck.sold)
 
 # Class Sub
-def sub(x, y):
-    return x - y
+# def sub(x, y):
+#     return x - y
 
-def sub2(x, y=0):
-    return x - y
+# def sub2(x, y=0):
+#     return x - y
 
 # print(sub2(7))
 
 # Light Class
-class light:
-    def __init__(self, sync=None):
-        print('creating light! ')
-        self.sync = sync
-        self.on = False
+# class light:
+#     def __init__(self, sync=None):
+#         print('creating light! ')
+#         self.sync = sync
+#         self.on = False
     
-    def toggle(self):
-        print('Toggle on and off!')
-        self.on = not self.on 
-        if self.sync is not None:
-            self.sync.toggle()
+#     def toggle(self):
+#         print('Toggle on and off!')
+#         self.on = not self.on 
+#         if self.sync is not None:
+#             self.sync.toggle()
     
-    def is_on(self):
-        if self.on == True:
-            print("Light is on!")
-        else:
-            print("Light is off!")
+#     def is_on(self):
+#         if self.on == True:
+#             print("Light is on!")
+#         else:
+#             print("Light is off!")
 
 # Light = light()
 # Light.is_on()
@@ -93,9 +93,67 @@ class light:
 # print("a.on: ", a.on)
 # print(Light.on)
 
-Light1 = light()
-Light2 = light(sync=Light1)
-Light1.is_on()
-Light2.toggle()
-Light1.is_on()
-Light2.is_on()
+# Light1 = light()
+# Light2 = light(sync=Light1)
+# Light1.is_on()
+# Light2.toggle()
+# Light1.is_on()
+# Light2.is_on()
+
+# Inheritance concept
+class IceCream:
+
+  max_scoops = 3 # add a class attribute (introduced before)
+
+  def __init__(self):
+    super().__init__()
+    self.scoops = 0
+
+  def eat(self, scoops):
+    if self.scoops < scoops:
+      print("Not enough bites left!")
+    else:
+      self.scoops -= scoops
+
+  def add(self, scoops):
+    self.scoops += scoops
+    if self.scoops > self.max_scoops: # current step - add logic
+      print("Too many scoops! Dropped ice cream.")
+      self.scoops = 0
+
+
+class IceCreamTruck: # current step - add IceCreamTruck class
+  
+  def __init__(self):
+    super().__init__()
+    self.sold = 0
+
+  def order(self, scoops):
+    ice_cream = IceCream()
+    self.add(ice_cream, scoops)
+    return ice_cream
+
+  def add(self, ice_cream, scoops):
+    ice_cream.add(scoops)
+    self.sold += scoops
+
+
+class DeluxeIceCreamTruck(IceCreamTruck):
+    
+    def order(self, scoops):
+        ice_cream = super().order(scoops)
+        ice_cream.add(1)
+        return ice_cream
+
+
+# truck = IceCreamTruck()
+# ice_cream1 = truck.order(3)
+# ice_cream1.eat(2)
+# truck.add(ice_cream1, 1)
+# print(truck.sold)
+
+truck = DeluxeIceCreamTruck()
+ice_cream = truck.order(2)
+
+print('Deluxe IceCream truck scoops: ', ice_cream.scoops)
+print(truck.sold)
