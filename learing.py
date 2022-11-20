d584c3f0d7608a89f1768cb1cdd7a5f263daa3f7
Arminas19 <armeska14@gmail.com>
2022-11-20 12:17:06 +0000

    experimented with MRO.

--- learing.py
+++ learing.py
@@ -57,6 +57,7 @@
 # Light Class
 # class light:
 #     def __init__(self, sync=None):
+#         super().__init__()
 #         print('creating light! ')
 #         self.sync = sync
 #         self.on = False
@@ -73,6 +74,29 @@
 #         else:
 #             print("Light is off!")
 
+
+# class OldLight(light):
+
+#   def __init__(self, sync=None):
+#     super().__init__(sync=sync)
+#     self.on = False
+#     self.sync = sync
+#     self.flicker = False
+  
+#   def toggle(self):
+#     super().toggle()
+#     if self.on:
+#       self.flicker = not self.flicker
+
+
+# light = OldLight()
+# print(light.flicker)
+# light.toggle()
+# print(light.flicker)
+# light.toggle()
+# print(light.flicker)
+# light.toggle()
+# print(light.flicker)
 # Light = light()
 # Light.is_on()
 # print("Light on?: ", Light.on)
@@ -101,49 +125,49 @@
 # Light2.is_on()
 
 # Inheritance concept
-class IceCream:
+# class IceCream:
 
-  max_scoops = 3 # add a class attribute (introduced before)
+#   max_scoops = 3 # add a class attribute (introduced before)
 
-  def __init__(self):
-    super().__init__()
-    self.scoops = 0
+#   def __init__(self):
+#     super().__init__()
+#     self.scoops = 0
 
-  def eat(self, scoops):
-    if self.scoops < scoops:
-      print("Not enough bites left!")
-    else:
-      self.scoops -= scoops
+#   def eat(self, scoops):
+#     if self.scoops < scoops:
+#       print("Not enough bites left!")
+#     else:
+#       self.scoops -= scoops
 
-  def add(self, scoops):
-    self.scoops += scoops
-    if self.scoops > self.max_scoops: # current step - add logic
-      print("Too many scoops! Dropped ice cream.")
-      self.scoops = 0
+#   def add(self, scoops):
+#     self.scoops += scoops
+#     if self.scoops > self.max_scoops: # current step - add logic
+#       print("Too many scoops! Dropped ice cream.")
+#       self.scoops = 0
 
 
-class IceCreamTruck:  # current step - add IceCreamTruck class
+# class IceCreamTruck:  # current step - add IceCreamTruck class
   
-  def __init__(self):
-    super().__init__()
-    self.sold = 0
+#   def __init__(self):
+#     super().__init__()
+#     self.sold = 0
 
-  def order(self, scoops):
-    ice_cream = IceCream()
-    self.add(ice_cream, scoops)
-    return ice_cream
+#   def order(self, scoops):
+#     ice_cream = IceCream()
+#     self.add(ice_cream, scoops)
+#     return ice_cream
 
-  def add(self, ice_cream, scoops):
-    ice_cream.add(scoops)
-    self.sold += scoops
+#   def add(self, ice_cream, scoops):
+#     ice_cream.add(scoops)
+#     self.sold += scoops
 
 
-class DeluxeIceCreamTruck(IceCreamTruck):
+# class DeluxeIceCreamTruck(IceCreamTruck):
     
-    def order(self, scoops):
-        ice_cream = super().order(scoops)
-        ice_cream.add(1)
-        return ice_cream
+#     def order(self, scoops):
+#         ice_cream = super().order(scoops)
+#         ice_cream.add(1)
+#         return ice_cream
 
 
 # truck = IceCreamTruck()
@@ -152,8 +176,19 @@ class DeluxeIceCreamTruck(IceCreamTruck):
 # truck.add(ice_cream1, 1)
 # print(truck.sold)
 
-truck = DeluxeIceCreamTruck()
-ice_cream = truck.order(2)
+# truck = DeluxeIceCreamTruck()
+# ice_cream = truck.order(2)
 
-print('Deluxe IceCream truck scoops: ', ice_cream.scoops)
-print(truck.sold)
+# print('Deluxe IceCream truck scoops: ', ice_cream.scoops)
+# print(truck.sold)
+
+# 2 parent classes
+
+class A:
+  pass
+
+class B(A):
+  pass
+
+class C(A, B):
+  pass
